a2600df4e7c2dc83165c97c0c6ec36641b752e59
Ayushhrajput <ayushrajput939@gmail.com>
2025-12-09 19:17:54 +0530

    changed bg-color

--- src/components/Mocktails.jsx
+++ src/components/Mocktails.jsx
@@ -52,22 +52,22 @@ function Mocktails(props) {
 
     return (
         <div className='h-dvh relative bg-blue-400' id='box'>
-            <img src={mocktails} alt="" id='img' className='w-60 lg:w-80 absolute left-1/2 -translate-x-1/2 -top-1/6' />
-            <div className="text-white text-lg w-max md:h-dvh md:w-6xl flex-col md:flex-row md:flex-center md:justify-around lg:items-end absolute left-1/2 -translate-x-1/2 bottom-1/8">
-                <div className='mocktails p-5 '>
-                    <h1 className='style-font text-blue-200'>Mocktails</h1>
+            <img src={mocktails} alt="" id='img' className='w-60 lg:w-80 absolute left-1/2 -translate-x-1/2 -top-1/4' />
+            <div className="bg-white text-lg w-max  md:w-6xl flex-col rounded-xl md:flex-row md:flex-center md:justify-around lg:items-end absolute left-1/2 -translate-x-1/2 bottom-1/8">
+                <div className='mocktails p-5 bg-gray-200 m-4 rounded-lg'>
+                    <h1 className='style-font text-gray-600'>Mocktails</h1>
                     <ul className=' flex flex-col  gap-4 md:gap-10'>
                         {mocktailsList.map(({name, country, detail, price}) => (
-                            <li key={name} className='list flex justify-between gap-4'><div>{name}({country})</div><p>{price}({detail})</p></li>
+                            <li key={name} className='list flex justify-between gap-4'><div>{name}({country})</div><p className='bg-white text-gray-600 px-4 rounded-2xl'>{price}({detail})</p></li>
                         ))}
                     </ul>
                 </div>
                 
-                <div className='cocktails p-5'>
-                    <h1 className='style-font text-blue-200'>Cocktails</h1>
+                <div className='cocktails p-5 bg-gray-200 m-4 rounded-lg'>
+                    <h1 className='style-font text-gray-600'>Cocktails</h1>
                     <ul className='flex flex-col gap-4 md:gap-10 '>
                         {cocktailsList.map(({name, country, detail, price}) => (
-                            <li key={name} className='list flex justify-between gap-4'><div>{name}({country})</div><p>{price}({detail})</p></li>
+                            <li key={name} className='list flex justify-between gap-4'><div>{name}({country})</div><p className='bg-white text-gray-600 px-4  rounded-2xl'>{price}({detail})</p></li>
                         ))}
                     </ul>
                 </div>
