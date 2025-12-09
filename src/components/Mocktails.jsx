3e663809e932cab2cce39866b8ebbc2938fd0a5b
Ayushhrajput <ayushrajput939@gmail.com>
2025-12-10 00:31:44 +0530

    deleted the mocktails section

--- src/components/Mocktails.jsx
+++ src/components/Mocktails.jsx
@@ -18,25 +18,7 @@ function Mocktails(props) {
     return (
         <div className='h-dvh relative bg-gray-800 ' id='box'>
             <h1 className='px-8 text-white text-2xl lg:text-4xl w-max h-min absolute  mt-2 md:top-1/6 style-font '>Mocktails</h1>
-            <div className="  w-max lg:w-6xl flex-col rounded-xl md:flex-row md:flex-center md:justify-around lg:items-end absolute left-1/2 -translate-x-1/2 bottom-1/8">
-                <div className='mocktails p-5 bg-gray-200 m-4 rounded-lg'>
-                    <h1 className='style-font text-gray-600'>Mocktails</h1>
-                    <ul className=' flex flex-col gap-4 md:gap-10'>
-                        {mocktailsList.map(({name, country, detail, price}) => (
-                            <li key={name} className='list flex justify-between gap-2'><div>{name}({country})</div><p className=' text-gray-600 px-4 rounded-2xl'>{price}({detail})</p></li>
-                        ))}
-                    </ul>
-                </div>
-                
-                <div className='cocktails p-5 bg-gray-200 m-4 rounded-lg'>
-                    <h1 className='style-font text-gray-600'>Cocktails</h1>
-                    <ul className='flex flex-col gap-4 md:gap-10 '>
-                        {cocktailsList.map(({name, country, detail, price}) => (
-                            <li key={name} className='list flex justify-between'><div>{name}({country})</div><p className= 'text-gray-600 px-4  rounded-2xl'>{price}({detail})</p></li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+            
         </div>
     );
 }
